31d541c50f44612f18b6d3d96a6baac9daf58211
Nanna Bernbom <nannaelisabeth@gmail.com>
2016-04-10 14:50:14 +0200

    more tests

--- code/upTri_test.py
+++ code/upTri_test.py
@@ -5,7 +5,12 @@ import upTri
 class TriTest(unittest.TestCase):
 
     def test_big_matrix(self):
-        A = np.matrix('-1 -1 -1 0 0 0 0 0; 1 0 0 -1 -1 -1 0 0; 0 1 0 1 0 0 -1 0; 0 0 1 0 1 0 1 -1; 0 0 0 0 0 1 0 1; 0 0 0 0 0 0 0 0')
+        A = np.matrix([[ -1, -1, -1, 0, 0, 0, 0, 0 ],
+            [ 1, 0, 0, -1, -1, -1, 0, 0 ],
+            [ 0, 1, 0, 1, 0, 0, -1, 0 ],
+            [ 0, 0, 1, 0, 1, 0, 1, -1 ],
+            [ 0, 0, 0, 0, 0, 1, 0, 1 ],
+            [ 0, 0, 0, 0, 0, 0, 0, 0 ]])
         B = np.matrix([[ 1,  0,  0, -1, -1,  0,  0,  1],
             [ 0,  1,  0,  1,  0,  0, -1,  0],
             [ 0,  0,  1,  0,  1,  0,  1, -1],
@@ -13,6 +18,17 @@ class TriTest(unittest.TestCase):
             [ 0,  0,  0,  0,  0,  0,  0,  0],
             [ 0,  0,  0,  0,  0,  0,  0,  0]])
         self.assertTrue((upTri.tri(A)==B).all)
+
+    def test_one_column(self):
+        A = np.matrix([[1],[-1],[1]])
+        B = np.matrix([[1],[0],[0]])
+        self.assertTrue((upTri.tri(A)==B).all)
+
+    def test_one_row(self):
+        A = np.matrix([[1, 1, 1]])
+        B = np.matrix([[1, 1, 1]])
+        self.assertTrue((upTri.tri(A)==B).all)
+
 
 if __name__=='__main__':
     unittest.main()
