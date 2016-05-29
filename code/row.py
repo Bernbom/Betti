43783e5a1f2b4cff1007381e9403b6411c4e7868
Nanna Bernbom <nannaelisabeth@gmail.com>
2016-05-29 21:26:24 +0200

    avoid copies where possible

--- code/row.py
+++ code/row.py
@@ -6,17 +6,17 @@ def swap(A,i,j):
     in matrix A: swap row i and j
     """
     temp = A.getrow(i)
-    A[i,:] = A.getrow(j)
+    A[i,:] = A.getrowview(j)
     A[j,:] = temp
 
 def scale(A,i,n):
     """
     in matrix A: multiply row i with n
     """
-    A[i,:] = n*A.getrow(i)   
+    A[i,:] *= n   
 
 def combine(A,i,j,n):
     """
     in matrix A: add row j to row i n times
     """
-    A[i,:] += n*A.getrow(j)
+    A[i,:] += n*A.getrowview(j)
