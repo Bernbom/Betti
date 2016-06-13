7c243588284fa6c21e64738ac7c5f88b0bf43234
Nanna Bernbom <nannaelisabeth@gmail.com>
2016-06-13 12:59:46 +0200

    more general

--- code/preproc.py
+++ code/preproc.py
@@ -8,16 +8,22 @@ def simcom_prep(off_file, target_file):
     in_file = open(off_file)
     out_file = open(target_file,'w')
 
-    if in_file.readline()!="OFF\n":
-        in_file.close()
-        out_file.close()
-        raise ValueError("first argument: " + off_file + " is not an OFF file")
-    else:
-        [vertices, faces, edges] = in_file.readline().rstrip('\n').split()
-        trash = in_file.readline() # empty line
+    offline = in_file.readline()
+    [vertices, faces, edges] = in_file.readline().rstrip('\n').split()
+    trash = in_file.readline() # possibly empty line
+    if trash == "\n":
         for vertice in xrange(int(vertices)):
             in_file.next()
             out_file.write(str(vertice) + ' ')
+        for face in xrange(int(faces)):
+            line = in_file.next()
+            line = line.rstrip('\n').split()
+            out_file.write(','.join(line[1:]) + ' ')
+    else:
+        for vertice in xrange(int(vertices)-1):
+            in_file.next()
+            out_file.write(str(vertice) + ' ')
+        out_file.write(str(vertice+1) + ' ')
         for face in xrange(int(faces)):
             line = in_file.next()
             line = line.rstrip('\n').split()
