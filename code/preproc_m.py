60c7994b99dc0ea3a890d7ba81e8ef09824e4e95
Nanna Bernbom <nannaelisabeth@gmail.com>
2016-06-14 21:51:53 +0200

    tranlates matlab vectors to the right format

---
+++ code/preproc_m.py
@@ -0,0 +1,21 @@
+import sys
+
+def simcom_prep(m_file, target_file):
+    """
+    given an off file containing a simplicial complex returns a file containing 
+    the simplicial complex in one line in a format ready for betti.py 
+    """
+    in_file = open(m_file)
+    out_file = open(target_file,'w')
+
+    for line in in_file:
+        l = line.rstrip('\n').split('\t')
+        out_file.write(','.join(l) + ' ')
+
+    # clean up
+    in_file.close()
+    out_file.close()
+
+if __name__ =="__main__":
+    simcom_prep(sys.argv[1],sys.argv[2])
+    print "success"
