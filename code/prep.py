ae63e7d117c03d8437b3fa9e9312b7f497203552
Nanna Bernbom <nannaelisabeth@gmail.com>
2016-06-14 21:54:55 +0200

    sort faces before combinating

--- code/prep.py
+++ code/prep.py
@@ -5,12 +5,13 @@ def prepare(simCom):
     given the maximal faces of a simplicial complex, as a string, creates a list 
     of strings reprecenting the faces in that complex 
     """
-    simCom = simCom.split(' ')
+    simCom = simCom.rstrip(' ').split(' ')
     s = set([])
     for i in xrange(len(simCom)):
         simCom[i] = simCom[i].split(',')
     # make faces
     for sim in simCom:
+        sim.sort()
         for l in range(1,len(sim)+1):
             s.update(it.combinations(iter(sim),l))
     s = list(s)
